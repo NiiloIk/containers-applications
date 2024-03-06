2a4fe891725f9133b3121383d0179e4ca44c40bc
Niilo I <niilo21002@student.hamk.fi>
2024-03-06 12:55:59 +0200

    lisätty osa tehtävistä

---
+++ todo-app/todo-backend/routes/todos.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const { Todo } = require('../mongo')
+const router = express.Router();
+
+/* GET todos listing. */
+router.get('/', async (_, res) => {
+  const todos = await Todo.find({})
+  res.send(todos);
+});
+
+/* POST todo to listing. */
+router.post('/', async (req, res) => {
+  const todo = await Todo.create({
+    text: req.body.text,
+    done: false
+  })
+  res.send(todo);
+});
+
+const singleRouter = express.Router();
+
+const findByIdMiddleware = async (req, res, next) => {
+  const { id } = req.params
+  req.todo = await Todo.findById(id)
+  if (!req.todo) return res.sendStatus(404)
+
+  next()
+}
+
+/* DELETE todo. */
+singleRouter.delete('/', async (req, res) => {
+  await req.todo.delete()  
+  res.sendStatus(200);
+});
+
+/* GET todo. */
+singleRouter.get('/', async (req, res) => {
+  res.send(req.todo)
+});
+
+/* PUT todo. */
+singleRouter.put('/', async (req, res) => {
+  try {
+    const body = req.body
+    const todo = await Todo.findById(req.todo.id)
+  
+    if (!todo) {
+      return res.status(404).send('Todo not found')
+    }
+  
+    todo.text = body.text || todo.text;
+    todo.done = body.done !== undefined ? body.done : todo.done;
+    
+    const updatedTodo = await todo.save();
+    res.send(updatedTodo)
+  } catch(error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+  
+});
+
+router.use('/:id', findByIdMiddleware, singleRouter)
+
+
+module.exports = router;
